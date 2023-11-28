dc761f3d5e079eec77e2843c39ac6b8c69e63e86
ThienAn03 <hvt91vuhoangthienan@gmail.com>
2023-11-28 22:03:31 +0700

    Update scheduler

--- source_code/Core/Src/scheduler.c
+++ source_code/Core/Src/scheduler.c
@@ -5,4 +5,71 @@
  *      Author: ASUS TUF GAMING
  */
 #include "scheduler.h"
+int TICK = 10;
 
+void SCH_Init ( void ){
+	for(int i = 0; i < SCH_MAX_TASKS; i++){
+		SCH_Delete_Task(i);
+	}
+}
+void SCH_Update( void ){
+	for(int i = 0; i < SCH_MAX_TASKS; i++){
+		if(SCH_tasks_G[i].pTask != 0){
+			if(SCH_tasks_G[i].Delay == 0){
+				SCH_tasks_G[i].RunMe += 1;
+				if(SCH_tasks_G[i].Period > 0){
+					SCH_tasks_G[i].Delay = SCH_tasks_G[i].Period;
+				}
+			}
+			else {
+				SCH_tasks_G[i].Delay--;
+			}
+		}
+	}
+}
+unsigned char SCH_Add_Task( void ( * pFunction) ( ) , unsigned int DELAY, unsigned int PERIOD){
+	int i;
+	for(i = 0; i<SCH_MAX_TASKS; i++){
+		if(SCH_tasks_G[i].pTask == 0) break;
+	}
+	if(i == SCH_MAX_TASKS){
+		//too many tasks
+		return SCH_MAX_TASKS;
+	}
+	SCH_tasks_G[i].pTask = pFunction;
+	SCH_tasks_G[i].Delay = DELAY/TICK;
+	SCH_tasks_G[i].Period = PERIOD/TICK;
+	SCH_tasks_G[i].RunMe = 0;
+
+	return i;
+}
+void SCH_Dispatch_Tasks ( void ){
+	for(int i = 0; i<SCH_MAX_TASKS; i++){
+		if(SCH_tasks_G[i].RunMe > 0){
+			//run task
+			(*SCH_tasks_G[i].pTask) ();
+			SCH_tasks_G[i].RunMe--;
+
+			if(SCH_tasks_G[i].Period <= 0){
+				SCH_Delete_Task(i);
+			}
+		}
+	}
+}
+unsigned char SCH_Delete_Task ( const int TASK_INDEX){
+	unsigned char return_code;
+	if(SCH_tasks_G[TASK_INDEX].pTask == 0){
+		return_code = 'F';
+	}
+	else {
+		return_code = 'T';
+	}
+
+	SCH_tasks_G[TASK_INDEX].pTask = 0;
+	SCH_tasks_G[TASK_INDEX].Delay = 0;
+	SCH_tasks_G[TASK_INDEX].Period = 0;
+	SCH_tasks_G[TASK_INDEX].RunMe = 0;
+	return return_code;
+}
+void SCH_Go_To_Sleep ();
+void SCH_Report_Status ( void );
